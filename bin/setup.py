05392f60937193a0b0a9fcfdfbd1ab06d7c59e02
Jason Scott <jgscott85@gmail.com>
2019-01-28 16:39:54 -0600

    Update executable location
    Changed file location of create-targets.py from /usr/local/bin to /usr/bin.

--- bin/setup.py
+++ bin/setup.py
@@ -8,7 +8,7 @@ dst = '/usr/share/man/man8'
 shutil.copyfile('{}'.format(os.path.abspath(os.path.join(src, '..', 'data', 'create-targets.8'))),
                 '/usr/share/man/man8/create-targets.8')
 os.system('gzip -f /usr/share/man/man8/create-targets.8')
-shutil.copyfile('{}/create-targets.py'.format(src), '/usr/local/bin/create-targets')
-os.system('sudo chmod +x /usr/local/bin/create-targets')
+shutil.copyfile('{}/create-targets.py'.format(src), '/usr/bin/create-targets')
+os.system('sudo chmod +x /usr/bin/create-targets')
 
 print("Complete.\n")
